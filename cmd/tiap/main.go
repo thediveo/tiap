28269b095b29e6eae12f6456eb80819067beed5f
TheDiveO <6920158+thediveo@users.noreply.github.com>
2023-05-26 23:40:57 +0200

    fixes missing cmd package

---
+++ cmd/tiap/main.go
@@ -0,0 +1,27 @@
+// Copyright 2023 by Harald Albrecht
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy
+// of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+package main
+
+import "os"
+
+func main() {
+	// This is cobra boilerplate documentation, except for the missing call to
+	// fmt.Println(err) which in the original boilerplate is just plain wrong:
+	// it renders the error message twice, see also:
+	// https://github.com/spf13/cobra/issues/304
+	if err := newRootCmd().Execute(); err != nil {
+		os.Exit(1)
+	}
+}
---
+++ cmd/tiap/root.go
@@ -0,0 +1,122 @@
+// Copyright 2023 by Harald Albrecht
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy
+// of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+	"path/filepath"
+	"runtime/debug"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/thediveo/tiap"
+	"golang.org/x/exp/slices"
+)
+
+const (
+	outnameFlag      = "out"
+	appVersionFlag   = "app-version"
+	releaseNotesFlag = "release-notes"
+	dockerHostFlag   = "host"
+)
+
+func buildInfo(info *debug.BuildInfo, key string) string {
+	idx := slices.IndexFunc(info.Settings,
+		func(setting debug.BuildSetting) bool {
+			return setting.Key == key
+		})
+	if idx < 0 {
+		return ""
+	}
+	return info.Settings[idx].Value
+}
+
+func newRootCmd() (rootCmd *cobra.Command) {
+	rootCmd = &cobra.Command{
+		Use:     "tiap [flags] [app-template-dir]",
+		Short:   "tiap isn't app publisher, but packages Industrial Edge .app files anyway",
+		Version: `":latest"`, // sorry :p
+		Args:    cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			log.Info("🗩  tiap ... isn't app publisher")
+
+			info, biok := debug.ReadBuildInfo()
+			if biok {
+				commit := buildInfo(info, "vcs.revision")
+				if commit != "" {
+					modified := ""
+					if buildInfo(info, "vcs.modified") == "true" {
+						modified = " (modified)"
+					}
+					log.Info(fmt.Sprintf("   %s commit %s%s",
+						info.Main.Version, commit[:8], modified))
+				}
+			}
+
+			log.Info("⚖  Apache 2.0 License")
+
+			semver, _ := rootCmd.Flags().GetString(appVersionFlag)
+			if semver == "" {
+				semverbytes, err := exec.Command("git", "describe").Output()
+				if err != nil {
+					return err
+				}
+				semver = strings.Trim(string(semverbytes), "\r\n")
+			}
+
+			releaseNotes, _ := rootCmd.Flags().GetString(releaseNotesFlag)
+
+			app, err := tiap.NewApp(args[0])
+			if err != nil {
+				return err
+			}
+			defer app.Done()
+
+			err = app.SetDetails(semver, releaseNotes)
+			if err != nil {
+				return err
+			}
+
+			host, _ := rootCmd.Flags().GetString(dockerHostFlag)
+			err = app.PullAndWriteCompose(context.Background(), host)
+			if err != nil {
+				return err
+			}
+
+			outname, _ := rootCmd.Flags().GetString(outnameFlag)
+			if filepath.Ext(outname) == "" {
+				outname = outname + ".app"
+			}
+			return app.Package(outname)
+		},
+	}
+	rootCmd.Flags().StringP(outnameFlag, "o", "",
+		"mandatory: name of app package file to write")
+	rootCmd.MarkFlagRequired("output")
+
+	rootCmd.Flags().String(appVersionFlag, "",
+		"app semantic version, defaults to git describe")
+
+	rootCmd.Flags().String(releaseNotesFlag, "",
+		"release notes")
+
+	rootCmd.Flags().StringP(dockerHostFlag, "H", "",
+		"Docker daemon socket to connect to")
+
+	return rootCmd
+}
